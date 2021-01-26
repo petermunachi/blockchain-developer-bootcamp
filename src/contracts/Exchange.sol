08fa4d352e53bff3a8ac554d47339ac58afce4ad
peter munachi <alimunachipeter@gmail.com>
2021-01-26 16:36:34 +0100

    completing smart contract backend

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -15,10 +15,10 @@ import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
 // [X] Deposit Tokens
 // [X] Withdraw Tokens
 // [X] Check balances
-// [ ] Make order
-// [ ] Cancel order
-// [ ] Fill order
-// [ ] Charge Fees
+// [X] Make order
+// [X] Cancel order
+// [X] Fill order
+// [X] Charge Fees
 
 
 
@@ -28,16 +28,35 @@ contract Exchange {
   using SafeMath for uint;
 
   // Variables
-  address public feeAccount; // the account that receives exchange fee account
+
   uint256 public feePercent; // the fee percentage
+  uint256 public orderCount;
+
   address constant ETHER = address(0); // store Ether in tokens mapping with blank address
+  address public feeAccount; // the account that receives exchange fee account
 
   mapping (address=>mapping (address=> uint256)) public tokens;
-
+  mapping (uint256=>_Order) public orders;
+  mapping (uint256=>bool) public orderCancelled;
+  mapping (uint256=>bool) public orderFilled;
 
   // Events
   event Deposit(address _token, address _user, uint256 _amount, uint256 _balance);
   event Withdraw(address _token, address _user, uint256 _amount, uint256 _balance);
+  event Order(uint256 id, address user, address tokenGet, uint256 amountGet, address tokenGive, uint256 amountGive, uint256 timestamp );
+  event Cancel(uint256 id, address user, address tokenGet, uint256 amountGet, address tokenGive, uint256 amountGive, uint256 timestamp );
+  event Trade(uint256 id, address user, address tokenGet, uint256 amountGet, address tokenGive, uint256 amountGive, address userFill, uint256 timestamp );
+
+  // Struct
+  struct _Order {
+    uint256 id;
+    address user;
+    address tokenGet;
+    uint256 amountGet;
+    address tokenGive;
+    uint256 amountGive;
+    uint256 timestamp;
+  }
 
 
   constructor (address _feeAccount, uint256 _feePercent) public {
@@ -85,6 +104,53 @@ contract Exchange {
 
   function balanceOf(address _token, address _user) view public returns (uint256) {
     return tokens[_token][_user];
+  }
+
+  function makeOrder(address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) public {
+    orderCount = orderCount.add(1);
+    orders[orderCount] = _Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, now);
+    emit Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, now);
+  }
+
+  function cancelOrder(uint256 _id) public {
+    _Order storage _order = orders[_id];
+    require(address(_order.user) == msg.sender);
+    require(_order.id == _id);
+    orderCancelled[_id] = true;
+    emit Cancel(_order.id, msg.sender, _order.tokenGet, _order.amountGet, _order.tokenGive, _order.amountGive, now);
+
+  }
+
+  function fillOrder(uint256 _id) public {
+    require(_id > 0 && _id <= orderCount);
+    require(!orderFilled[_id]);
+    require(!orderCancelled[_id]);
+    _Order storage _order = orders[_id];
+    _trade(_order.id, _order.user, _order.tokenGet, _order.amountGet, _order.tokenGive, _order.amountGive);
+    orderFilled[_order.id] = true;
+  }
+
+  function _trade(uint256 _orderId, address _user, address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) internal {
+    // Fee paid by the user that fills the order, a.k.a msg.sender
+    // Fee deducted from _amountGet
+    uint256 _feeAmount = _amountGive.mul(feePercent).div(100);
+
+    // Execute trade
+
+    //remove tokens and fee from the sender(the person filling the order) of contract
+    tokens[_tokenGet][msg.sender] = tokens[_tokenGet][msg.sender].sub(_amountGet.add(_feeAmount));
+    //add tokens to the user(the person who created the order) 
+    tokens[_tokenGet][_user] = tokens[_tokenGet][_user].add(_amountGet);
+
+    // add fee to the deployer of the contract
+    tokens[_tokenGet][feeAccount] = tokens[_tokenGet][feeAccount].add(_feeAmount);
+
+    //remove ether from the user(the person who created the order) 
+    tokens[_tokenGive][_user] = tokens[_tokenGive][_user].sub(_amountGive);
+    //add ether to the sender(the person filling the order) 
+    tokens[_tokenGive][msg.sender] = tokens[_tokenGive][msg.sender].add(_amountGive);
+
+    emit Trade(_orderId, _user, _tokenGet, _amountGet, _tokenGive, _amountGive, msg.sender, now);
   }
 
 
